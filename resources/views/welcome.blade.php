251ffe53a80c493d3374d67d1d8ea3bda37e071c
mortypk <mortypk@gmail.com>
2023-01-22 10:13:52 +0500

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,6 +23,20 @@
         </style>
     </head>
     <body class="antialiased">
+        
+    <x-theme.nav.bar class="mb-1">
+        <x-theme.nav.logo>Munshi</x-theme.nav.logo>
+        <x-theme.nav.link>Home</x-theme.nav.link>
+        <x-theme.nav.link>About</x-theme.nav.link>
+        <x-theme.nav.link>Contact</x-theme.nav.link>
+        <x-theme.nav.dropdown copation="Product">
+            <x-theme.nav.dropdown.link>Dress</x-theme.nav.dropdown.link>
+            <x-theme.nav.dropdown.link>Food</x-theme.nav.dropdown.link>
+            <x-theme.nav.dropdown.link>Electronic</x-theme.nav.dropdown.link>
+        </x-theme.nav.dropdown>
+        <div class="flex-grow"></div>
+        <x-theme.nav.link>Login</x-theme.nav.link>
+    </x-theme.nav.bar>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./lang/en/theme.php",
     "./resources/**/*.blade.php",
