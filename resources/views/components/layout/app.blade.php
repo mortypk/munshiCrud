cc7a86268898ef721a382b3440d0a9e6174ac726
mortypk <mortypk@gmail.com>
2023-01-22 15:10:05 +0500

    update

---
+++ resources/views/components/layout/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <!-- Style and Scripts -->
+        @vite( ['resources/css/app.css', 'resources/js/app.js'])
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <livewire:styles />
+    </head>
+    <body class="bg-gray-100 dark:bg-gray-900">
+        {{ $slot }}
+        <livewire:scripts />
+    </body>
+</html>
---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,13 @@
+<x-theme.nav.bar class="mb-1">
+    <x-theme.nav.logo>Munshi</x-theme.nav.logo>
+    <x-theme.nav.link>Home</x-theme.nav.link>
+    <x-theme.nav.link>About</x-theme.nav.link>
+    <x-theme.nav.link>Contact</x-theme.nav.link>
+    <x-theme.nav.dropdown copation="Product">
+        <x-theme.nav.dropdown.link>Dress</x-theme.nav.dropdown.link>
+        <x-theme.nav.dropdown.link>Food</x-theme.nav.dropdown.link>
+        <x-theme.nav.dropdown.link>Electronic</x-theme.nav.dropdown.link>
+    </x-theme.nav.dropdown>
+    <div class="flex-grow"></div>
+    <x-theme.nav.link>Login</x-theme.nav.link>
+</x-theme.nav.bar>
